bc56962421397d11c38bb61bf8ab3bc8897727a1
Patrick Gustafson <patrickgus@charter.net>
2019-12-27 16:24:01 -0800

    install helmet

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const helmet = require("helmet");
 const POKEDEX = require("./pokedex.json");
 
 console.log(process.env.API_TOKEN);
@@ -9,6 +10,7 @@ console.log(process.env.API_TOKEN);
 const app = express();
 
 app.use(morgan("dev"));
+app.use(helmet());
 app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
